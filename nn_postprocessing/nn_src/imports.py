7dde6f8a62963204ad032ab88bdbd98c5bf3da13
Stephan Rasp <raspstephan@gmail.com>
2018-04-01 17:11:40 -0700

    Tried pruning of input features, still some work to do!

--- nn_postprocessing/nn_src/imports.py
+++ nn_postprocessing/nn_src/imports.py
@@ -1,5 +1,5 @@
 import numpy as np
-import sys
+import sys, os, pdb, pickle, time
 from .utils import *
 from .losses import *
 from .keras_models import * 
@@ -11,7 +11,8 @@ import seaborn as sns
 from tqdm import tqdm_notebook as tqdm
 from collections import OrderedDict
 from IPython.display import HTML
-import time
 from keras.utils.generic_utils import get_custom_objects
 metrics_dict = dict([(f.__name__, f) for f in [crps_cost_function]])
 get_custom_objects().update(metrics_dict)
+from timeit import default_timer
+from keras.callbacks import EarlyStopping
