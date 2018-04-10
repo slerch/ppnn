991d61dca0907d71afbad1329bddd79100bb26a3
slerch <Sebastian.Lerch@kit.edu>
2018-04-10 13:27:25 +0200

    update data preparation and qrf code

--- qrf_postprocessing/qrf_global.R
+++ qrf_postprocessing/qrf_global.R
@@ -5,29 +5,44 @@
 rm(list=ls())
 
 data_dir <- "/media/sebastian/Elements/Postproc_NN/data/"
-load(paste0(data_dir, "data_prep_2015-16.Rdata"))
+load(paste0(data_dir, "data_all.Rdata"))
 
-# remove sm_mean and sm_var due to missing values
-data_train$sm_mean <- NULL
-data_train$sm_var <- NULL
-data_eval$sm_mean <- NULL
-data_eval$sm_var <- NULL
+## ignore sm_mean and sm_var due to missing and NA values leading to problems in estimation
+data$sm_mean <- NULL
+data$sm_var <- NULL
 
 library(quantregForest)
 library(scoringRules)
+library(lubridate)
 
-# tuning parameters based on code by Maxime Taillardat
-# estimation takes ~ 3 hours on my laptop
-qrF_model <- quantregForest(x = data_train[,2:(ncol(data_train)-2)], y = data_train$obs,
-                            mtry = ceiling(ncol(data_train[,2:(ncol(data_train)-2)])/3),
-                            nodesize = 10,
-                            ntree = 250,
-                            importance = TRUE)
+start_eval <- as.Date("2016-01-01 00:00", tz = "UTC")
+end_eval <- as.Date("2016-12-31 00:00", tz = "UTC") 
 
-# with default parameters:
-# qrF_model_default <- quantregForest(x = data_train[,2:(ncol(data_train)-2)], 
-#                                     y = data_train$obs)
-  
+data_eval_all <- subset(data, date >= start_eval & date <= end_eval)
+
+end_train <- start_eval - hours(48)
+start_train <- as.Date("2015-01-01 00:00", tz = "UTC")
+
+qt_levels <- seq(1/51, 50/51, by = 1/51)
+qts_save <- matrix(NA, 
+                   nrow = nrow(data_eval_all), 
+                   ncol = length(qt_levels))
+
+data_train <- subset(data, date >= start_train & date <= end_train)
+data_train <- data_train[complete.cases(data_train),]
+
+data_eval <- subset(data, date >= start_eval & date <= end_eval)
+
+# model
+qrF_model <- quantregForest(x = data_train[, !(names(data_train) %in% c("date", "station", "obs"))], 
+                            y = data_train$obs)
+
+# compute quantiles on evaluation data
+qrF_prediction <-   predict(qrF_model,
+                            newdata = data_eval[, !(names(data_train) %in% c("date", "station", "obs"))],
+                            what = qt_levels,
+                            all = TRUE)
+
 qt_levels <- seq(1/51, 50/51, by = 1/51)
 qts_save <- matrix(NA, nrow = nrow(data_eval), ncol = length(qt_levels))
 
@@ -36,6 +51,7 @@ qrF_prediction <-   predict(qrF_model,
                             what = qt_levels,
                             all = TRUE)
 
+# Evaluation
 ind_noNAinRow <- which(!apply(qrF_prediction, 1, anyNA))
 ind_use <- intersect(which(!is.na(data_eval$obs)), ind_noNAinRow)
 
@@ -44,12 +60,8 @@ qrf_crps <- crps_sample(y = data_eval$obs[ind_use],
 
 summary(qrf_crps)
 
-## with nodesize = 10, ntree = 250; mean CRPS 0.9890
-## with default parameters: mean CRPS 0.9869
-## tuning parameters do not seem to make a big difference! 
-
+# Variable importance
 qrF_model$importance
 importance(qrF_model, type=1, scale=TRUE)
 
-
-# save(qrF_model, file = "qrF_model.Rdata")
+plot(importance(qrF_model, type=1, scale=TRUE))
--- qrf_postprocessing/qrf_local.R
+++ qrf_postprocessing/qrf_local.R
@@ -6,21 +6,32 @@
 rm(list=ls())
 
 data_dir <- "/media/sebastian/Elements/Postproc_NN/data/"
-load(paste0(data_dir, "data_prep_2015-16.Rdata"))
+load(paste0(data_dir, "data_all.Rdata"))
 
 ## ignore sm_mean and sm_var due to missing and NA values leading to problems in estimation
-data_train$sm_mean <- NULL
-data_train$sm_var <- NULL
-data_eval$sm_mean <- NULL
-data_eval$sm_var <- NULL
+data$sm_mean <- NULL
+data$sm_var <- NULL
 
 library(quantregForest)
 library(scoringRules)
+library(lubridate)
 
+# train and eval dates (any date in date = valid date of fc and!)
+start_eval <- as.Date("2016-01-01 00:00", tz = "UTC")
+end_eval <- as.Date("2016-12-31 00:00", tz = "UTC") 
+
+data_eval_all <- subset(data, date >= start_eval & date <= end_eval)
+
+end_train <- start_eval - hours(48)
+start_train <- as.Date("2015-01-01 00:00", tz  = "UTC")
+
+# quantile levels
 qt_levels <- seq(1/51, 50/51, by = 1/51)
-qts_save <- matrix(NA, nrow = nrow(data_eval), ncol = length(qt_levels))
+qts_save <- matrix(NA, 
+                   nrow = nrow(data_eval_all), 
+                   ncol = length(qt_levels))
 
-stations <- unique(data_eval$station)
+stations <- unique(data$station)
 
 for(this_station in stations){
   # progress indicator
@@ -29,39 +40,46 @@ for(this_station in stations){
     cat(progind, "of", length(stations), "started at", paste(Sys.time()), "\n")
   }
   
-  ind_this_station <- which(data_train$station == this_station)
+  data_train <- subset(data,
+                       date >= start_train & date <= end_train & station == this_station)
+ 
+  data_train <- data_train[complete.cases(data_train),]
+  
+  data_eval <- subset(data,
+                      date >= start_eval & date <= end_eval & station == this_station)
+  # need to delete cases with NA obs in eval data to avoid error in qrf package
+  # (even though not needed in predict function...)
+  data_eval$obs <- NULL
   
   # omit stations with too few observation to successfully estimate model
-  if(length(ind_this_station) <= 10){next}
+  if(nrow(data_train) <= 10){next}
   
   # estimate model on training data
-  data_train_thisstation <- subset(data_train, station == this_station)
-  
-  qrF_model <- quantregForest(x = data_train_thisstation[,2:(ncol(data_train)-2)], 
-                              y = data_train_thisstation$obs,
-                              mtry = ceiling(ncol(data_train_thisstation[,2:(ncol(data_train)-2)])/3),
+  qrF_model <- quantregForest(x = data_train[, !(names(data_train) %in% c("date", "station", "obs"))], 
+                              y = data_train$obs,
                               nodesize = 5,
-                              ntree = 250,
-                              importance = FALSE)
+                              ntree = 500)
   
   # compute quantiles on evaluation data
   qrF_prediction <-   predict(qrF_model,
-                              newdata = subset(data_eval, station == this_station),
+                              newdata = data_eval,
                               what = qt_levels,
                               all = TRUE)
   
-  ind_this_station_eval <- which(data_eval$station == this_station)
   
-  qts_save[ind_this_station_eval,] <- qrF_prediction
+  # position of fc cases for eval data
+  # to save qrF prediction to row in qts_save
+  qts_save[which(data_eval_all$station == this_station),] <- qrF_prediction
 }
 
 
 ind_noNAinRow <- which(!apply(qts_save, 1, anyNA))
-ind_use <- intersect(which(!is.na(data_eval$obs)), ind_noNAinRow)
+ind_use <- intersect(which(!is.na(data_eval_all$obs)), ind_noNAinRow)
 
-dim(qts_save)[1] - length(ind_use)
+dim(qts_save)[1] - length(ind_use) # all NA cases in all eval data
+dim(qts_save)[1] - length(ind_use) - sum(is.na(data_eval_all$obs)) # NA cases due to model estimation in eval data
 
-qrf_crps <- crps_sample(y = data_eval$obs[ind_use],
+qrf_crps <- crps_sample(y = data_eval_all$obs[ind_use],
                         dat = qts_save[ind_use,])
 
 summary(qrf_crps)
@@ -72,11 +90,6 @@ summary(qrf_crps)
 # all to default: --> mean CRPS 0.9972
 # nodesize = 5, ntree = 500 --> mean CRPS 0.9882
 # nodesize = 15, ntree = 500 --> mean CRPS 1.0083
+
+# 0.8215!!
 # nodesize = 5, ntree = 1000 --> 0.9875
-
-
-vr <- sapply(data_eval$obs[ind_use],
-             function(i) rank(c(data_eval$obs[ind_use][i], 
-                                qts_save[ind_use,][i, ]),
-                              ties.method = "random")[1])
-
