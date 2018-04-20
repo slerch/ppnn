ca54201b104d8ec1523a594d58b717419862e52d
slerch <Sebastian.Lerch@kit.edu>
2018-04-20 09:41:40 +0200

    document bug in train split for qrf

--- qrf_postprocessing/qrf_global_alldata_testSplit.R
+++ qrf_postprocessing/qrf_global_alldata_testSplit.R
@@ -2,6 +2,14 @@
 ## proposed by Taillardat et al (2016, Monthly Weather Review)
 ## partly based on R code provided by Maxime Taillardat
 
+##
+## running global QRF model for all data requires splitting training data
+## and fitting multiple models on random subsets
+## However:
+## bug in "combine()" function for quantregForest objects
+## I have contacted the author of the package who will look for a solution
+##
+
 rm(list=ls())
 
 data_dir <- "/media/sebastian/Elements/Postproc_NN/data/"
@@ -31,21 +39,10 @@ qts_save <- matrix(NA,
 data_train <- subset(data, date >= start_train & date <= end_train)
 data_train <- data_train[complete.cases(data_train),]
 
-# ## splitting into a specific number of parts here
-# ## however, this is less straightforward compared to test how large indiv parts can be, see below
-# ## split data_train into 10 seperate RF models, and combine them later on
-# ## this could also be done randomly by permuting the indices first
-# permuted_rowIndices <- sample(1:nrow(data_train), size = nrow(data_train), replace = FALSE)
-# nparts <- 100
-# # following row would split into parts of size npart, which is not what we want
-# # rowInd_splitted <- split(permuted_rowIndices, ceiling(seq_along(permuted_rowIndices)/nparts))
-# # instead:
-# part_size <- floor(length(permuted_rowIndices)/nparts)
-# rowInd_splitted <- split(permuted_rowIndices, ceiling(seq_along(permuted_rowIndices)/part_size))
-# str(rowInd_splitted)
-# # drop last list entry so that other ones have the same size 
-# rowInd_splitted[[nparts+1]] <- NULL
-# str(rowInd_splitted)
+# split up data set into smaller chunks
+# and fit rF model on each chunk individually
+# this avoids lack of memory issues when fitting models with large data sets
+# also allows straightforward parallelization of experiments on cluster
 
 ## split data_train into parts of size say 5000
 ## this could also be done randomly by permuting the indices first
@@ -58,25 +55,65 @@ rowInd_splitted[[length(rowInd_splitted)]] <- NULL
 str(rowInd_splitted)
 
 
-# now, we have a list of indices to train separate qrF models on
-rF_list <- list()
+# # now, we have a list of indices to train separate qrF models on
+# rF_list <- list()
+# nparts <- length(rowInd_splitted)
+# for(ID in 1:nparts){
+#   
+#   cat(ID, "of", nparts, "started at", paste(Sys.time()), "\n")
+#   rows_use <- rowInd_splitted[[ID]]
+#   data_train_use <- data_train[rows_use,]
+#   
+#   qrF_model <- quantregForest(x = data_train_use[!(names(data_train) %in% c("date", "station", "obs"))], 
+#                               y = data_train_use$obs,
+#                               ntree = 1000,
+#                               nodesize = 5,
+#                               nthreads = 1,
+#                               mtry = 20,
+#                               replace = TRUE)
+#   
+#   rF_list[[ID]] <- qrF_model
+# }
+# 
+# 
+# rF_model_comb <- do.call("combine", rF_list)
+
+# better:
+
 nparts <- length(rowInd_splitted)
-for(ID in 1:nparts){
-  
+
+fit_model <- function(ID){
   cat(ID, "of", nparts, "started at", paste(Sys.time()), "\n")
   rows_use <- rowInd_splitted[[ID]]
   data_train_use <- data_train[rows_use,]
   
   qrF_model <- quantregForest(x = data_train_use[!(names(data_train) %in% c("date", "station", "obs"))], 
                               y = data_train_use$obs,
-                              ntree = 1000,
+                              ntree = 250,
                               nodesize = 5,
                               nthreads = 1,
                               mtry = 20,
                               replace = TRUE)
   
-  rF_list[[ID]] <- qrF_model
+  return(qrF_model)
 }
 
+rF_list <- lapply(as.list(1:nparts), fit_model)
 
 rF_model_comb <- do.call("combine", rF_list)
+
+## should work like this, however, combined model results in strangely sharp predictions,
+## or even only 0's for all quantiles
+
+data_eval <- subset(data, date >= start_eval & date <= end_eval)
+data_eval <- data_eval[1:500,]
+
+nn <- 20
+qt_levels <- seq(1/(nn+1), nn/(nn+1), by = 1/(nn+1)) # quantile levels
+
+qrF_prediction <-   predict(rF_model_comb,
+                            newdata = data_eval[, !(names(data_eval) %in% c("date", "station", "obs"))],
+                            what = qt_levels,
+                            all = TRUE)
+
+head(qrF_prediction)
